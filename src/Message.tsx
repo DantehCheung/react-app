c7bbde0728287341b0fc91a19ad4bb10af1004a6
DantehCheung <dantehcheung@gmail.com>
2024-10-07 01:52:00 +0800

    first commit

---
+++ src/Message.tsx
@@ -0,0 +1,14 @@
+
+
+// PascalCasing follow when create function
+function Message(){
+    // JSX: JavaScript XML
+    // allow use easily to create dynamic content
+
+    const name = 'Danteh';
+    if(name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>
+
+}
+export default Message;
